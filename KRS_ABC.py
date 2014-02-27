f953800b20b7d3e84ef11a5817b794b5ad6a6ffd
Steven Laan <therealneojume@gmail.com>
2014-02-27 14:50:47 +0100

    Semi-hacked version

--- KRS_ABC.py
+++ KRS_ABC.py
@@ -16,15 +16,16 @@ import matplotlib.pyplot as plt
 def plot_krs(xs, ts, h, rng, y_star):
     means = np.zeros(50)
     stds = np.zeros(50)
+    Ns = np.zeros(50)
     confs = np.zeros(50)
     for i, val in enumerate(rng):
-        means[i], stds[i], confs[i], _ = kernel_regression(val, xs, ts, h)
+        means[i], stds[i], confs[i], Ns[i] = kernel_regression(val, xs, ts, h)
 
     
     plt.fill_between(
             rng,
-            means - confs, 
-            means + confs, 
+            means - stds / np.sqrt(Ns), 
+            means + stds / np.sqrt(Ns), 
             color=[0.7, 0.3, 0.3, 0.5])
     plt.fill_between(
             rng, 
@@ -37,7 +38,6 @@ def plot_krs(xs, ts, h, rng, y_star):
     plt.axhline(y_star)
     plt.ylim(-2,4)
     plt.title('S = {0}, h = {1}'.format(len(xs), h))
-    plt.show()
 
 def conditional_error(alphas, u, tau, M):
     if u <= tau:
@@ -45,7 +45,7 @@ def conditional_error(alphas, u, tau, M):
     else:
         return float(np.sum(alphas >= u)) / M
 
-def KRS_ABC(problem, num_samples, epsilon, ksi, S0, delta_S):
+def KRS_ABC(problem, num_samples, epsilon, ksi, S0, delta_S, verbose=False):
     
     M = 50
     E = 50    
@@ -71,9 +71,9 @@ def KRS_ABC(problem, num_samples, epsilon, ksi, S0, delta_S):
     samples = np.zeros(num_samples)
     
     xs = list(prior.rvs(*prior_args, N=S0))
-    ts = [simulator(x)[0] for x in xs]
+    ts = [simulator(x) for x in xs]
     
-    sim_calls = 0
+    sim_calls = S0
     accepted = 0
     
     h = np.var(xs) * pow(3.0 / (4 * len(xs)), 0.2) / 8.0
@@ -81,22 +81,23 @@ def KRS_ABC(problem, num_samples, epsilon, ksi, S0, delta_S):
     rng = np.linspace(-np.pi, 5 * np.pi)
 
     for i in xrange(num_samples):
-        if i % 100 == 0:
-            print i, sim_calls
-            #plot_krs(xs, ts, h, rng, y_star)
+        if verbose:
+            if i % 100 == 0:
+                print i, sim_calls
+                #plot_krs(xs, ts, h, rng, y_star)
             
         # Sample theta_p from proposal
-        theta_p = prior.rvs(*prior_args)#proposal.rvs(theta, *proposal_args)
+        theta_p = proposal.rvs(theta, *proposal_args)
         #log_theta_p = np.log(theta_p)
         
         while True:
             # Get mus and sigmas from Kernel regression
-            mu_bar, sigma_j, sigma, N = kernel_regression(theta, xs, ts, h)
-            mu_bar_p, sigma_j_p, sigma_p, N_p = kernel_regression(theta_p, xs, ts, h)
+            mu_bar, std, _, N = kernel_regression(theta, xs, ts, h)
+            mu_bar_p, std_p, _, N_p = kernel_regression(theta_p, xs, ts, h)
            
             # Get samples
-            mu = distr.normal.rvs(mu_bar, sigma / N, M)
-            mu_p = distr.normal.rvs(mu_bar_p, sigma_p / N_p, M)
+            mu = distr.normal.rvs(mu_bar, std / np.sqrt(N), M)
+            mu_p = distr.normal.rvs(mu_bar_p, std_p / np.sqrt(N_p), M)
             
             # Compute alpha using eq. 19
             numer = prior.logpdf(theta_p, *prior_args) + \
@@ -107,8 +108,8 @@ def KRS_ABC(problem, num_samples, epsilon, ksi, S0, delta_S):
             alphas = np.zeros(M)
             for m in xrange(M):
                 other_term = \
-                    distr.normal.logpdf(y_star, mu_p[m], sigma_j_p + (epsilon ** 2)) - \
-                    distr.normal.logpdf(y_star, mu[m], sigma_j + (epsilon ** 2))        
+                    distr.normal.logpdf(y_star, mu_p[m], std_p + (epsilon ** 2)) - \
+                    distr.normal.logpdf(y_star, mu[m], std + (epsilon ** 2))
             
                 log_alpha = min(0.0, (numer - denom) + other_term)
                 alphas[m] = np.exp(log_alpha)
@@ -118,24 +119,23 @@ def KRS_ABC(problem, num_samples, epsilon, ksi, S0, delta_S):
             error = np.mean([e * conditional_error(alphas, e, tau, M) \
                     for e in np.linspace(0, 1, E)])        
 
-            #print alphas
-            #print tau
-            #print error
-            #raw_input()
-            
             if error > ksi:
                 # Acquire training point
                 
                 # For now we add training points at theta and theta_p
-                new_x = prior.rvs(*prior_args)
+                new_x = problem.prior.rvs(*prior_args)
+                #new_x = distr.uniform.rvs(min(theta, theta_p), max(theta, theta_p))
                 ts.append(simulator(new_x)[0])
                 xs.append(new_x)
-                
+
                 h = np.var(xs) * pow(3.0 / (4 * len(xs)), 0.2) / 8.0
 
                 sim_calls += 1
-                #if sim_calls % 300 == 0:
-                #    plot_krs(xs, ts, h, rng, y_star)
+                if sim_calls % 500 == 0:
+                    plot_krs(xs, ts, h, rng, y_star)
+                    plt.axvline(theta)
+                    plt.axvline(theta_p)
+                    plt.show()
             else:
                 break
             
@@ -156,12 +156,12 @@ def KRS_ABC(problem, num_samples, epsilon, ksi, S0, delta_S):
 if __name__ == '__main__':
     from problems import toy_problem, sinus_problem
     problem = sinus_problem()
-    samples, rate, sim_calls = KRS_ABC(problem, 10000, 0.1, 0.05, 20, 10)
+    samples, rate, sim_calls = KRS_ABC(problem, 1000, 0.0, 0.1, 20, 10)
     
     print sim_calls, rate   
     
     #rng = np.linspace(0.07, 0.13)
-    plt.hist(samples[1500:], bins = 100, normed=True)
+    plt.hist(samples, bins = 100, normed=True)
     #plt.plot(rng, np.exp(distr.gamma.logpdf(rng, 0.1 + 500, (0.1 + 500*9.42))))
 
     plt.show()
