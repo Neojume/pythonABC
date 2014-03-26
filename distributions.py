f8edd9021d5d4b730817895f06db068c14ba51c4
Steven Laan <therealneojume@gmail.com>
2014-03-26 13:34:10 +0100

    Added two multidimensional distributions

--- distributions.py
+++ distributions.py
@@ -48,6 +48,7 @@ class proportional(object):
             val = quad(self.pdf, -np.inf, x)[0]
         return val
 
+
 class gamma(object):
     @staticmethod
     def pdf(x, alpha, beta):
@@ -70,21 +71,6 @@ class gamma(object):
         return np.random.gamma(alpha, 1.0 / beta, N)
 
 
-class normal(object):
-    @staticmethod
-    def pdf(x, mu, sigma):
-        return np.exp(normal.logpdf(x, mu, sigma))
-
-    @staticmethod
-    def logpdf(x, mu, sigma):
-        return - 0.5 * np.log(2.0 * np.pi) - np.log(sigma) - \
-            0.5 * ((x - mu) ** 2) / (sigma ** 2)
-
-    @staticmethod
-    def rvs(mu, sigma, N=1):
-        return np.random.normal(mu, sigma, N)
-
-
 class exponential(object):
     @staticmethod
     def pdf(x, beta):
@@ -102,24 +88,6 @@ class exponential(object):
         return np.random.exponential(1.0 / beta, N)
 
 
-class uniform(object):
-    @staticmethod
-    def pdf(x, a=0, b=1):
-        #if a <= x <= b:
-        return (np.all([a <= x, x <= b], axis=0)) / (b - a)
-        #return 0.0
-
-    @staticmethod
-    def logpdf(x, a=0, b=1):
-        if a <= x <= b:
-            return -np.log(b - a)
-        return - np.inf
-
-    @staticmethod
-    def rvs(a=0, b=1, N=1):
-        return np.random.uniform(a, b, N)
-
-
 class lognormal(object):
     @staticmethod
     def pdf(x, mu, sigma):
@@ -127,18 +95,18 @@ class lognormal(object):
 
     @staticmethod
     def logpdf(x, mu, sigma):
-        if x.__class__ == np.ndarray:
+        if type(x) == np.ndarray:
             if sigma > 0:
                 small = np.log(0.5 + 0.5 * special.erf((np.log(1e-6) - mu) /
                                (np.sqrt(2.0) * sigma)))
                 I = pp.find(x > 1e-6)
                 log_x = np.log(x[I])
-                lp = small*np.ones(x.shape)
+                lp = small * np.ones(x.shape)
                 lp[I] = -log_x - 0.5 * np.log(2.0 * np.pi) - np.log(sigma) - \
                     0.5 * ((log_x - mu) ** 2) / (sigma ** 2)
             else:
                 I = pp.find(x == mu)
-                lp = -np.inf*np.ones(x.shape)
+                lp = -np.inf * np.ones(x.shape)
                 lp[I] = 0
         else:
             if sigma > 0:
@@ -176,3 +144,76 @@ class logitnormal(object):
     @staticmethod
     def rvs(mu, sigma):
         pass
+
+
+class uniform(object):
+    @staticmethod
+    def pdf(x, a=0, b=1):
+        #if a <= x <= b:
+        return (np.all([a <= x, x <= b], axis=0)) / (b - a)
+        #return 0.0
+
+    @staticmethod
+    def logpdf(x, a=0, b=1):
+        if a <= x <= b:
+            return -np.log(b - a)
+        return - np.inf
+
+    @staticmethod
+    def rvs(a=0, b=1, N=1):
+        return np.random.uniform(a, b, N)
+
+
+class uniform_nd(object):
+    @staticmethod
+    def pdf(x, p1, p2):
+        float(np.all(x > p1) and np.all(x < p2)) / np.prod(p1 - p2)
+
+    @staticmethod
+    def logpdf(x, p1, p2):
+        if np.all(x > p1) and np.all(x < p2):
+            return -np.sum(np.log(p1 - p2))
+        return -np.inf
+
+    @staticmethod
+    def rvs(p1, p2):
+        return np.random.uniform(p1, p2)
+
+
+class normal(object):
+    @staticmethod
+    def pdf(x, mu, sigma):
+        return np.exp(normal.logpdf(x, mu, sigma))
+
+    @staticmethod
+    def logpdf(x, mu, sigma):
+        return -0.5 * np.log(2.0 * np.pi) - np.log(sigma) - \
+            0.5 * ((x - mu) ** 2) / (sigma ** 2)
+
+    @staticmethod
+    def rvs(mu, sigma, N=1):
+        return np.random.normal(mu, sigma, N)
+
+
+class multivariate_normal(object):
+    @staticmethod
+    def pdf(x, mu, sigma):
+        return np.exp(multivariate_normal.logpdf(x, mu, sigma))
+
+    @staticmethod
+    def logpdf(x, mu, sigma):
+        m = np.matrix(x - mu)
+        det = np.linalg.det(sigma)
+        inv = np.linalg.inv(sigma)
+        try:
+            k = x.shape[-1]
+        except AttributeError:
+            # Floats have no shape attribute
+            k = 1
+        return -0.5 * (m * inv * m.T + k * np.log(2 * np.pi) + np.log(det))
+
+    @staticmethod
+    def rvs(mu, sigma, N=1):
+        return np.random.multivariate_normal(mu, sigma, N)
+
+# TODO: Implement more multidimensional distributions
